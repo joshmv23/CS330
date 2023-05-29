fdc374900b5610f842b316db23ead62e623af4fb
Josh Morfin-Vazquez <127642400+joshmv23@users.noreply.github.com>
2023-05-29 02:50:13 -0700

    Create response.cpp

---
+++ homework05/response.cpp
@@ -0,0 +1,83 @@
+/*
+  Implementation of methods for classes Response, AngryResponse, HappyResponse
+*/
+#include <iostream>
+#include <string>
+#include <algorithm>
+
+#include "response.h"
+
+using namespace std;
+
+/*
+  Implementation of Word class
+  Don't worry too hard about these implementations.
+  If you'd like to learn more about STL see: 
+    https://www.geeksforgeeks.org/the-c-standard-template-library-stl/
+*/
+string Word::upper()
+{
+  string result(theWord);
+  transform(result.begin(), result.end(), result.begin(), ::toupper);
+  return result;
+}
+
+string Word::lower()
+{
+  string result(theWord);
+  transform(result.begin(), result.end(), result.begin(), ::tolower);
+  return result;
+}
+
+/*
+  Implementation of Response methods
+*/
+bool Response::checkAndRespond(const string& inWord, ostream& toWhere)
+{
+    // TODO:
+    // This method should check if the current object's keyword is in the
+    // input message (inWord), and send the proper response to the toWhere
+    // output stream
+    // String class provides the function 'find' which you might find useful
+    size_t index = inWord.find(keyword.upper()); //find position of keyword
+    if(index != string::npos){ //check to make sure keyword is in input message
+        respond(toWhere); //add it to the output stream(toWhere) to make it gives the appropriate response
+        return true; 
+    }
+    return false;
+}
+
+void Response::respond(ostream& toWhere)
+{
+    // TODO:
+    // This method should 'insert' "I am neither angry nor happy: " followed by
+    // the object's response word to the toWhere output stream, along with
+    // a newline at the end
+    toWhere << "I am neither angry nor happy: " << response << endl;
+}
+
+
+void AngryResponse::respond(ostream& toWhere)
+{
+  // TODO:
+  // Implement the 'respond' member function for the AngryResponse class so that
+  // the angry rseponse "I am angry: " followed by the object's response word 
+  // is inserted to the toWhere output stream, along with a newline at the end
+  toWhere << "I am angry: " << response << endl;
+}
+
+
+
+/*
+  Implementation of HappyResponse methods
+*/
+
+void HappyResponse::respond(ostream& toWhere){
+  // TODO:
+  // Implement the 'respond' member function for the HappyResponse class so that
+  // the happy rseponse "I am happy: " followed by the object's response word 
+  // is inserted to the toWhere output stream, along with a newline at the end
+  toWhere << "I am happy: " << response << endl;
+}
+
+
